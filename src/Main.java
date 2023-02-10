27dc33a0806facfff4b536bd20277edf6c6cadf6
AlexanderElkin1969 <eaa@mfcmmr.ru>
2023-02-10 03:06:24 +0300

    task done

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Привет, МИР!!!");
-        System.out.println("Hello World!!!");
+        String [] str = {"Привет",", ","МИР","Привет","МИР", "!","МИР","Привет","МИР", "!"};
+        for (int i = 0; i < str.length; i++) {
+            if (str[i].equals("")) continue;
+            for (int j = i+1; j < str.length ; j++) {
+                if (str[i].equals(str[j])) {
+                    str[j] = "";
+                }
+            }
+            System.out.print(str[i]);
+        }
     }
 }
