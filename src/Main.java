6db568c74e61b2bd40fd066aeab71c1f1300e535
AlexanderElkin1969 <eaa@mfcmmr.ru>
2023-02-13 15:08:13 +0300

    remade_StringBuilder

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
         String[] str = {"Привет", ", ", "МИР", "Привет", "МИР", "!", "МИР", "Привет", "МИР", "!"};
         String rezult = "";
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < str.length; i++) {
             if (str[i].equals("")) continue;
             for (int j = i + 1; j < str.length; j++) {
@@ -12,7 +13,9 @@ public class Main {
             }
             System.out.print(str[i]);
             rezult +=  str[i];
+            sb.append(str[i]);
         }
         System.out.println("\n" + rezult);
+        System.out.println(sb);
     }
 }
