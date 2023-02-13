a0893244b343f8ccc07e91500e69554add654285
AlexanderElkin1969 <eaa@mfcmmr.ru>
2023-02-13 14:28:49 +0300

    remade

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,18 @@
 public class Main {
 
     public static void main(String[] args) {
-        String [] str = {"Привет",", ","МИР","Привет","МИР", "!","МИР","Привет","МИР", "!"};
+        String[] str = {"Привет", ", ", "МИР", "Привет", "МИР", "!", "МИР", "Привет", "МИР", "!"};
+        String rezult = "";
         for (int i = 0; i < str.length; i++) {
             if (str[i].equals("")) continue;
-            for (int j = i+1; j < str.length ; j++) {
+            for (int j = i + 1; j < str.length; j++) {
                 if (str[i].equals(str[j])) {
                     str[j] = "";
                 }
             }
             System.out.print(str[i]);
+            rezult +=  str[i];
         }
+        System.out.println("\n" + rezult);
     }
 }
